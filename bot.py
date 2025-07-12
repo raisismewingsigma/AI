f20c12f459453e740e7dc2636d55bc55e7c685a9
rais <juangsamiarani@gmail.com>
2025-07-13 06:07:02 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,38 +1,59 @@
 import discord
 from discord.ext import commands
-from config import token  # Import the bot's token from configuration file
+from config import token
+from logic import Pokemon , magic , fighter
+import random
 
-intents = discord.Intents.default()
-intents.members = True  # Allows the bot to work with users and ban them
-intents.message_content = True
+# Setting up intents for the bot
+intents = discord.Intents.default()  # Getting the default settings
+intents.messages = True              # Allowing the bot to process messages
+intents.message_content = True       # Allowing the bot to read message content
+intents.guilds = True                # Allowing the bot to work with servers (guilds)
 
+# Creating a bot with a defined command prefix and activated intents
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+# An event that is triggered when the bot is ready to run
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')
+    print(f'Logged in as {bot.user.name}')  # Outputs the bot's name to the console
 
+# The '!go' command
 @bot.command()
-async def start(ctx):
-    await ctx.send("Hi! I'm a chat manager bot!")
-
-@bot.command()
-@commands.has_permissions(ban_members=True)
-async def ban(ctx, member: discord.Member = None):
-    if member:
-        if ctx.author.top_role <= member.top_role:
-            await ctx.send("It is not possible to ban a user with equal or higher rank!")
+async def go(ctx):
+    author = ctx.author.name  # Getting the name of the message's author
+    # Check whether the user already has a Pokémon. If not, then...
+    if author not in Pokemon.pokemons.keys():
+        chance = random.randint(1,3)
+        if chance == 1:
+            pokemon = Pokemon(author)
+        elif chance == 2:
+            pokemon = magic(author)
+        elif chance == 3:
+            pokemon = fighter(author)
+        await ctx.send(await pokemon.info())  # Sending information about the Pokémon
+        image_url = await pokemon.show_img()  # Getting the URL of the Pokémon image
+        if image_url:
+            embed = discord.Embed()  # Creating an embed message
+            embed.set_image(url=image_url)  # Setting up the Pokémon's image
+            await ctx.send(embed=embed)  # Sending an embedded message with an image
         else:
-            await ctx.guild.ban(member)
-            await ctx.send(f"User {member.name} was banned.")
+            await ctx.send("Failed to upload an image of the pokémon.")
     else:
-        await ctx.send("This command should point to the user you want to ban. For example: `!ban @user`")
+        await ctx.send("You've already created your own Pokémon.")  # A message that is printed whether a Pokémon has already been created
 
-@ban.error
-async def ban_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send("You do not have sufficient permissions to execute this command.")
-    elif isinstance(error, commands.MemberNotFound):
-        await ctx.send("User not found.")
-
+@bot.command()
+async def attack(ctx):
+    target = ctx.message.mentions[0] if ctx.message.mentions else None
+    if target:
+        if target.name in Pokemon.pokemons and ctx.author.name in Pokemon.pokemons:
+            enemy = Pokemon.pokemons(target.name)
+            attacker = Pokemon.pokemons(ctx.author.name)
+            result = await attacker.attack(enemy)
+            await ctx.send(result)
+        else:
+            await ctx.send("kedua trainer harus punya pokemon yang akan digunakan!")
+    else:
+        await ctx.send("pilih lawan mu!,sebutkan nama usernya!")
+# Running the bot
 bot.run(token)
